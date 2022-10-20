a1835434ef9c38988ffb2b3a070ae8de6a7e6482
damien vincent <vincent.damien@gmail.com>
2022-10-20 12:46:14 +0200

    End of logical exercices

---
+++ 1-pseudo_code.rb
@@ -0,0 +1,40 @@
+def sum_of_greater_than_20(integers)
+    # cette méthode retourne la somme de tous les entiers > 20
+    # écrire le PSEUDO CODE ici (et pas de code): 
+
+end
+
+# exemple d'appel :
+puts sum_of_greater_than_20( [17, 18, 20, 21, 2, 23] )
+# returns 44
+
+def merge_2_arrays(array1, array2)
+    # cette méthode fusionne 2 arrays de même taille et retourne un array composés des élements
+    # du 1er array en alternance avec ceux du 2eme
+    # écrire le PSEUDO CODE ici (et pas de code): 
+
+end
+
+# exemple d'appel :
+puts merge_2_arrays( ["a", "c", "e"], ["b", "d", "f"])
+# retourne ["a", "b", "c", "d", "e", "f"]
+
+def even_numbers_until(limit)
+        # cette méthode retourne la liste des chiffre pairs de 0 à limit
+        # écrire le PSEUDO CODE ici (et pas de code): 
+end
+
+# exemple d'appel :
+puts even_numbers_until(9)
+# retourne [0, 2, 4, 6, 8]
+
+def delete_half_words(sentence)
+    # cette méthode prend une phrase en argument et retourne la même phrase amputée d'un mot sur 2
+    # écrire le PSEUDO CODE ici (et pas de code): 
+end
+
+puts delete_half_words("le ruby est un language de programmation génial")
+# retourne "ruby un de génial"
+
+
+
